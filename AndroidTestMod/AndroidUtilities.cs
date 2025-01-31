91342bb02734c3aa14152860b0100d70e73bad15
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-01-31 10:31:48 -0500

    what the sigma

---
+++ AndroidTestMod/AndroidUtilities.cs
@@ -0,0 +1,80 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AndroidTestMod;
+
+[BepInAutoPlugin("dev.xtracube.androidutilities")]
+public partial class AndroidUtilities : BasePlugin
+{
+    public static ManualLogSource Logger { get; private set; }
+    public static AndroidUtilities Instance { get; private set; }
+    public ConfigEntry<int> TargetFrameRate { get; private set; }
+    public ConfigEntry<LightSourceRendererType> LightSourceRenderMode { get; private set; }
+
+    public AndroidUtilities()
+    {
+        Instance = this;
+        Logger = Log;}
+    
+    public override void Load()
+    {
+        TargetFrameRate = Config.Bind("General", "Target Frame Rate", 60, "The target frame rate of the game");
+        LightSourceRenderMode = Config.Bind("General", "Light Source Mode", LightSourceRendererType.GPU, "The renderer type of the light source");
+        Config.Save();
+
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Id);
+    }
+
+    [HarmonyPatch(typeof(AmongUsClient), nameof(AmongUsClient.Awake))]
+    public static class AmongUsClientPatch
+    {
+        public static void Postfix()
+        { 
+            Application.targetFrameRate = Instance.TargetFrameRate.Value;
+        }
+    }
+
+    [HarmonyPatch(typeof(LightSource), nameof(LightSource.Initialize))]
+    public static class LightSourcePatch
+    {
+        public static void Prefix(LightSource __instance)
+        {
+            __instance.rendererType = Instance.LightSourceRenderMode.Value;
+        }
+    }
+
+    [HarmonyPatch(typeof(HatManager), nameof(HatManager.Initialize))]
+    public static class HatManagerPatch
+    {
+        public static void Postfix(HatManager __instance)
+        {
+            CosmeticsUnlocker.UnlockCosmetics(__instance);
+        }
+    }
+
+    [HarmonyPatch(typeof(EOSManager))]
+    public static class EosPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(EOSManager.StartInitialLoginFlow))]
+        public static bool InitialLoginPrefix(EOSManager __instance)
+        {
+            __instance.StartTempAccountFlow();
+            __instance.CloseStartupWaitScreen();
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(EOSManager.LoginFromAccountTab))]
+        public static bool LoginFromAccountTabPrefix(EOSManager __instance)
+        {
+            __instance.StartTempAccountFlow();
+            return false;
+        }
+    }
+}
